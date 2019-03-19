142ba8758ec6f28d06d0ce6358ec0a26b9276ad1
dingximing <dingximing@shhuzhong.com>
2019-03-19 17:07:49 +0800

    [ADD] three way to implement max swap

--- max_swap/max_swap.c
+++ max_swap/max_swap.c
@@ -51,16 +51,13 @@ int maximumSwap(int num) {
     for(i=0; i<n; i++){
         int maxn = arr[i];
         int index = i;
-        //int flag = false; // 多余
         for(j=i+1; j<n; j++){
             if(arr[j] >= maxn){
-                //flag = true;
                 index = j;
                 maxn = arr[j];
             }
         }
         if(maxn > arr[i]){
-            printf("=== swap ===");
             swap(&arr[i], &arr[index]);
             //交换过则退出
             return toInt(arr, n);
@@ -69,13 +66,101 @@ int maximumSwap(int num) {
     return num;
 }
 
-int main(int argc, char *argv[]) {
-    int num = 897214;
-    printf("origin number: %d\n", num);
-    printf("swap number: %d\n", maximumSwap(num));
+/*
 
-    num = 127294;
-    printf("origin number: %d\n", num);
-    printf("swap number: %d\n", maximumSwap(num));
+这个方法的思路在于：如果有一个低位数字比高位数字要大，那么交换它们肯定能使得当前数变得更大。为了在交换后得到最大的数，不但需要：与可能的最高位交换，也需要：确保你交换到最高位的数要尽量足够大。
+
+算法如下:
+
+从最低位一直遍历到最高位，存储迄今为止最大的数字。
+
+如果当前数字比迄今为止最大的数字小，那么存储swap索引：当前数字的索引 和 迄今为止最大数字的索引。
+如果当前数字比迄今为止最大的数字大，那么重置 迄今为止最大的数字。
+
+最后，交换之前存储的两个swap索引。重新计算数字。
+*/
+
+int maximumSwapVersion2(int num) {
+    int arr[20] = {0};
+    int back = num, n = 0;
+    while(back){
+        arr[n++] = back % 10;
+        back /= 10;
+    }
+    int i, j;
+    int maxn = arr[0];
+    int index = 0;
+    int max_index = 0;
+    //9774821 , 98878323
+    for(i=1; i < n; i++) {
+        if(maxn < arr[i]) {
+          for(j = i+1; j < n; j++) {
+            if(arr[j] < arr[i]) {
+                maxn = arr[i];
+                index = i;
+                max_index = i;
+            }
+          }
+        } else if(maxn > arr[i]){
+            index = i; 
+        }
+    }
+
+    if (max_index != index) {
+        swap(&arr[index], &arr[max_index]);
+    }
+
+    int ret=0;
+    for(i=n; i > 0; i--) {
+        ret = ret*10 + arr[i-1];
+    }
+    return ret;
+}
+
+int maximumSwapVersion3(int num) {
+    //988823437
+    //arr[0..n]={7,3,4,3,2,8,8,8,9}
+    int arr[20] = {0};
+    int back = num, n = 0;
+    while(back){
+        arr[n++] = back % 10;
+        back /= 10;
+    }
+    int bucket[10] = {-1};
+    int i, j;
+    for(i=0, j=n-1; i<j; i++, j--)
+        swap(&arr[i], &arr[j]);
+
+    for(i=0; i<n; i++) {
+        bucket[arr[i]] = i;     
+    }
+    //118923234
+    //bucket[1] = 1
+    //bucket[9] = 3
+    int index =0;
+    int max_index = 0;
+    for(i=0; i<n; i++){
+        for(j=9; j>0; j--) {
+            if(bucket[j]>0 && bucket[j] > i && j > arr[i]) {
+               index = i; 
+               max_index = bucket[j];
+               swap(&arr[index], &arr[max_index]);
+               return toInt(arr,n);
+            } 
+        }
+    }
+    return num;
+}
+
+
+int main(int argc, char *argv[]) {
+    int array[] = {98889292,432143532, 32314489, 7279797, 118923234, 98882701};
+    int i;
+    for(i = 0; i < sizeof(array)/sizeof(int); i++) {
+        printf("origin number: %30d\n", array[i]);
+        printf("swap number:     %30d\n", maximumSwap(array[i]));
+        printf("swap number(V2): %30d\n", maximumSwapVersion2(array[i]));
+        printf("swap number(V3): %30d\n", maximumSwapVersion3(array[i]));
+    }
     return 0;
 }
